c5cb82b25c3fb357f7bd8f4f544bbaef08c22f84
gajjas <gajjas@rpi.edu>
2020-11-07 11:05:46 -0500

    Tabs

--- app.py
+++ app.py
@@ -1,13 +1,64 @@
 import dash
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_table as dt
+from dash.dependencies import Input, Output
 
 # Initialise the app
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+tabs = html.Div(
+    [
+        dbc.Tabs(
+            [
+                dbc.Tab(label="Tab 1", tab_id="tab-1"),
+                dbc.Tab(label="Tab 2", tab_id="tab-2"),
+            ],
+            id="tabs",
+            active_tab="tab-1",
+        ),
+        html.Div(id="content"),
+    ]
+)
+
+tab1_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.P("This is tab 1!", className="card-text"),
+            dbc.Button("Click here", color="success"),
+        ]
+    ),
+    className="mt-3",
+)
+
+tab2_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.P("This is tab 2!", className="card-text"),
+            dbc.Button("Don't click here", color="danger"),
+        ]
+    ),
+    className="mt-3",
+)
+
+@app.callback(Output("content", "children"), [Input("tabs", "active_tab")])
+def switch_tab(at):
+    if at == "tab-1":
+        return tab1_content
+    elif at == "tab-2":
+        return tab2_content
+    return html.P("This shouldn't ever be displayed...")
 
 # Define the app
-app.layout = html.Div(
+app.layout = html.Div([
+    html.Div("Cov Data", id="title"),
+    html.Div([
+        tabs
 
-)
+    ],id="MapSection"),
+    html.Div([],id="generic-section"),
+    html.Div([],id="hospital-div")
+], style={"margin": "auto", "width": "50%","padding": "10px"})
 
 
 
